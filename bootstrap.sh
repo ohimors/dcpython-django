9d841d6db27ebcbee16cd116d281a1cc8cf3fc9d
David Greisen <dgreisen@gmail.com>
2013-10-04 17:37:17 -0400

    database work
    bootstrap creates user, db
    
    added heroku settings
    
    added initial data for dev
    
    still can't run make.py syncdb from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,20 @@
+export PYTHONPATH=$PYTHONPATH:/vagrant
+
+# update everything
 sudo apt-get update
 sudo apt-get upgrade
+
+# install Heroku
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# install apt packages
 sudo apt-get install postgresql postgresql-server-dev-all python-dev python-pip -y
+
+# install all python modules in requirements.txt
 sudo pip install -r /vagrant/requirements.txt
 
-export PYTHONPATH=$PYTHONPATH:/vagrant
+# setup postgresql
+sudo -u postgres createuser --superuser vagrant
+sudo -u postgres psql -c "alter user postgres with password '1234';"
+sudo -u postgres psql -c "CREATE DATABASE dcpython;"
+/vagrant/manage.py syncdb
--- dcpython/settings.py
+++ dcpython/settings.py
@@ -1,3 +1,5 @@
+import os
+
 # Django settings for dcpython project.
 
 DEBUG = True
@@ -9,21 +11,29 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dcpython',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'vagrant',
+        'PASSWORD': '1234',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
 
+if "DATABASE_URL" in os.environ:
+    DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -61,7 +71,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles' 
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,6 +83,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -156,3 +168,9 @@ LOGGING = {
         },
     }
 }
+
+# load sample data
+if "DATABASE_URL" in os.environ:
+    FIXTURE_DIRS = (
+       '/vagrant/dcpython/fixtures',
+    )
