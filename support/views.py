07d96613e2696b32ecee00e1a6d4eb919304fc57
vagrant <vagrant@precise32.(none)>
2013-10-07 21:08:07 +0000

    donations page taking shape

--- support/views.py
+++ support/views.py
@@ -5,16 +5,18 @@ from django.template import RequestContext
 # Create your views here.
 
 def support(request):
-    return render(request, 'support/support.html', {"active": "donate"})
+    context = RequestContext(request)
+    context.update({"active": "donate", "form": DonorForm()})
+    return render(request, 'support/support.html', context)
 
 def donor_update(request, secret=None):
-	donor = get_object_or_404(Donor, secret=secret)
-	if request.method == 'POST':
-		form = DonorForm(request.POST, instance=donor)
-		if form.is_valid():
-			form.save()
-	else:
-		form = DonorForm(instance=donor)
-	context = RequestContext(request)
-	context.update({"secret": secret, "form": form, "name": donor.name})
-	return render(request, 'support/donor_update.html', context)
+    donor = get_object_or_404(Donor, secret=secret)
+    if request.method == 'POST':
+        form = DonorForm(request.POST, instance=donor)
+        if form.is_valid():
+            form.save()
+    else:
+        form = DonorForm(instance=donor)
+    context = RequestContext(request)
+    context.update({"secret": secret, "form": form, "name": donor.name})
+    return render(request, 'support/donor_update.html', context)
