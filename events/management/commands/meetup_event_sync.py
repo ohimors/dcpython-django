f2a9aab332310ae02e12038aa899e3745932e26c
Chris Adams <chris@improbable.org>
2013-10-09 17:39:56 -0400

    Events: local sync
    Model refactor:
    * Ensure that meetup IDs are unique
    * Add auto_now / auto_now_add timestamps to Events
    * Change meetup_id columns to text to match API responses
    * Add meetup_event_sync management command
    * Update field blank status based on existing data

---
+++ events/management/commands/meetup_event_sync.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from events.models import Event
+
+
+class Command(BaseCommand):
+    help = 'Synchronizes the local event database with Meetup.com'
+
+    def handle(self, *args, **options):
+        try:
+            import bpdb as pdb
+        except ImportError:
+            import pdb
+
+        try:
+            Event.sync_from_meetup()
+        except Exception as exc:
+            self.stderr.write("ERROR: %s" % exc)
+            pdb.pm()
--- events/models.py
+++ events/models.py
@@ -1,57 +1,73 @@
+# encoding: utf-8
+from __future__ import absolute_import
+
+from itertools import chain
+
 from django.db import models
 
-# Create your models here.
+from app.integration.meetup import get_upcoming_events, get_past_events
+
 
 class Venue(models.Model):
-    meetup_id = models.IntegerField()
-    zip_code = models.CharField(max_length=5)
-    lon = models.IntegerField()
-    lat = models.IntegerField()
-    repinned = models.BooleanField()
+    meetup_id = models.CharField(unique=True, max_length=32)
+
+    # TODO: switch to GeoDjango PointField?
+    lon = models.DecimalField(null=True, max_digits=9, decimal_places=6)
+    lat = models.DecimalField(null=True, max_digits=9, decimal_places=6)
+
     name = models.CharField(max_length=200)
     state = models.CharField(max_length=2)
     address_1 = models.CharField(max_length=200)
-    address_2 = models.CharField(max_length=200)
-    address_3 = models.CharField(max_length=200)
+    address_2 = models.CharField(max_length=200, blank=True)
+    address_3 = models.CharField(max_length=200, blank=True)
     city = models.CharField(max_length=100)
-    country=models.CharField(max_length=2)
+    country = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5, blank=True)
+
+    repinned = models.BooleanField()
+
+    @classmethod
+    def create_from_meetup(cls, meetup_data):
+        i, created = cls.objects.get_or_create(meetup_id=meetup_data['id'])
+
+        for k, v in meetup_data.items():
+            if k == "id": continue
+            setattr(i, k, v)
+
+        i.full_clean()
+        i.save()
+        return i
 
 
 class Event(models.Model):
-    meetup_id = models.IntegerField()
-    utc_offset = models.IntegerField()
-    duration = models.IntegerField()
-    time = models.DateTimeField()
-    last_updated = models.DateTimeField()
-    created = models.DateTimeField()
-    meetup_url = models.URLField()
-    description = models.TextField()
+    record_created = models.DateTimeField(auto_now_add=True)
+    record_modified = models.DateTimeField(auto_now=True)
+
+    meetup_id = models.CharField(unique=True, max_length=32)
+
     name = models.CharField(max_length=200)
-    venue = models.ForeignKey("Venue")
+    venue = models.ForeignKey("Venue", null=True, blank=True)
 
-    def update_from_meetup_data(self, meetup_data):
-        """
-        Compare json with existing
-        Check against last_updated
-        """
+    start_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True, blank=True)
 
+    meetup_url = models.URLField(blank=True)
 
-class EventManager(models.Manager):
-    """
-    Custom Event Manager for Event model
-    """
+    description = models.TextField(blank=True, null=True)
 
-    def create_event_from_meetup_data(self, meetup_data):
-        """
-        Create new event from meetup data
-        """
-    
-    def update_event_from_meetup_data(self, meetup_data):
-        """
-        Look up ID, get model, and call Model.update_from_meetup_data
-        """
+    @classmethod
+    def sync_from_meetup(cls):
+        for i in chain(get_past_events(), get_upcoming_events()):
+            event, created = cls.objects.get_or_create(meetup_id=i['id'])
 
-    def update_or_create_event_from_meetup_data(self, meetup_data):
-        """
-        Get ID and see if it exists. If exists call update else create
-        """
+            for j in ('name', 'description', 'start_time', 'end_time'):
+                setattr(event, j, i.get(j))
+
+            event.meetup_url = i['event_url']
+
+            venue = i.get('venue')
+            if venue:
+                event.venue = Venue.create_from_meetup(venue)
+
+            event.full_clean()
+            event.save()
